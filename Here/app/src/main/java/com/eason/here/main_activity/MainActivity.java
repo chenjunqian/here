9fbf4cecb634e0df932ef2173f8d2ae360a312c3
Eason <cjq_noon@sina.com>
2015-08-24 23:06:45 +0800

    Fix Toolbar android support problem

--- Here/app/src/main/java/com/eason/here/main_activity/MainActivity.java
+++ Here/app/src/main/java/com/eason/here/main_activity/MainActivity.java
@@ -1,11 +1,12 @@
 package com.eason.here.main_activity;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.eason.here.R;
 
@@ -13,31 +14,62 @@ import com.eason.here.R;
 public class MainActivity extends Activity {
 
     private DrawerLayout drawerLayout;
-    private ToggleButton topBtn;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
     }
 
-    private void initView(){
-        drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
-        topBtn = (ToggleButton)findViewById(R.id.main_top_menu_button);
+    private void initView() {
+        toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.action_settings){
 
-        topBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    drawerLayout.openDrawer(Gravity.LEFT);
-                }else{
-                    drawerLayout.closeDrawers();
-                }
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu();
             }
-        });
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
     }
 
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
