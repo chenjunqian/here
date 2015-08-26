4556e67bc5c60b4eda491322daff15d0d3ca25ba
Eason <cjq_noon@sina.com>
2015-08-26 21:51:58 +0800

    set mysql database

--- HereServer/HereServer/settings.py
+++ HereServer/HereServer/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'HereServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'csvt',
+        'USER':'root',
+        'PASSWORD':'70233374',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
