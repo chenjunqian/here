efbf51860347db465420356d3f148b51245f3df9
Chenjunqian <cjq_noon@sina.com>
2015-09-01 19:03:39 +0800

    2015-9-1
    display blue arrow
    but still cannot get real location

--- Here/app/src/main/java/com/eason/here/main_activity/MainActivity.java
+++ Here/app/src/main/java/com/eason/here/main_activity/MainActivity.java
@@ -1,37 +1,63 @@
 package com.eason.here.main_activity;
 
 import android.content.res.Configuration;
+import android.location.Location;
+import android.location.LocationManager;
+import android.content.Context;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.graphics.Color;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.LocationSource;
+import  com.amap.api.location.AMapLocationListener;
+import  com.amap.api.location.AMapLocation;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
 import com.eason.here.BaseActivity;
 import com.eason.here.R;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.MyLocationStyle;
 
 
-public class MainActivity extends BaseActivity {
+
+public class MainActivity extends BaseActivity implements LocationSource,AMapLocationListener{
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private android.support.v7.widget.Toolbar toolbar;
+    private MapView mapView;
+    private AMap aMap;
+    private OnLocationChangedListener mListener;
+    private LocationManagerProxy mAMapLocationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-
+        initView(savedInstanceState);
     }
-
     /*
         初始化控件
      */
-    private void initView() {
+    private void initView(Bundle savedInstanceState) {
         toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        //显示地图
+        mapView = (MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            setUpMap();
+        }
         //将toolbar与Drawerlayout绑定起来
         actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,
@@ -53,27 +79,103 @@ public class MainActivity extends BaseActivity {
 
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
     }
-
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
-
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         actionBarDrawerToggle.onConfigurationChanged(newConfig);
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+    private void setUpMap() {
+        // 自定义系统定位小蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.drawable.location_marker));// 设置小蓝点的图标
+        myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));// 设置圆形的填充颜色
+        //myLocationStyle.anchor(int,int)//设置小蓝点的锚点
+        myLocationStyle.strokeWidth(1.0f);// 设置圆形的边框粗细
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        //aMap.setMyLocationType()
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+        deactivate();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+    @Override
+    public void onLocationChanged(AMapLocation aLocation) {
+        if (mListener != null && aLocation != null) {
+            mListener.onLocationChanged(aLocation);// 显示系统小蓝点
+        }
+    }
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mAMapLocationManager == null) {
+            mAMapLocationManager = LocationManagerProxy.getInstance(this);
+			/*
+			 * mAMapLocManager.setGpsEnable(false);
+			 * 1.0.2版本新增方法，设置true表示混合定位中包含gps定位，false表示纯网络定位，默认是true Location
+			 * API定位采用GPS和网络混合定位方式
+			 * ，第一个参数是定位provider，第二个参数时间最短是2000毫秒，第三个参数距离间隔单位是米，第四个参数是定位监听者
+			 */
+            mAMapLocationManager.requestLocationData(
+                    LocationProviderProxy.AMapNetwork, 2000, 10, this);
+        }
+    }
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mAMapLocationManager != null) {
+            mAMapLocationManager.removeUpdates(this);
+            mAMapLocationManager.destroy();
+        }
+        mAMapLocationManager = null;
     }
 }
