0a55bb20e2ccf59fd1a18004d843c5108a63d022
Eason <cjq_noon@sina.com>
2015-12-23 17:08:29 +0800

    about us page

---
+++ Here/app/src/main/java/com/eason/here/about_us_activity/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package com.eason.here.about_us_activity;
+
+import android.os.Bundle;
+
+import com.eason.here.BaseActivity;
+
+/**
+ * Created by Eason on 12/23/15.
+ */
+public class AboutUsActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
